3574258e67050f0f65f99b99153d9a2087df19fb
Boy-Brahmanda03 <boybrahmanda03@gmail.com>
2024-11-14 12:19:20 +0800

    add env and setting config to cloud

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	server: {
+		port: parseInt(import.meta.env.VITE_PORT) || 3000, // Menggunakan port dari env atau 3000
+		host: true, // Mendengarkan di semua alamat IP, diperlukan untuk server
+	},
+});
